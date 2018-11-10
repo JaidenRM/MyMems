44445e83a65e8e0979488f3b587ecdbc7c0d7680
Jaiden Muscat <muscatj@student.unimelb.edu.au>
2018-11-10 13:42:00 +1100

    added integration with mainactivity and postactivity TODO: added camera & images functionality

--- app/src/main/java/com/github/jaidenrm/mymems/MainActivity.java
+++ app/src/main/java/com/github/jaidenrm/mymems/MainActivity.java
@@ -1,7 +1,10 @@
 package com.github.jaidenrm.mymems;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -9,11 +12,25 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button newPost;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         Toast.makeText(this, user.getDisplayName(), Toast.LENGTH_SHORT).show();
+        InitUI();
+    }
+
+    private void InitUI() {
+        newPost = findViewById(R.id.main_newPost_button);
+        newPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), PostActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
